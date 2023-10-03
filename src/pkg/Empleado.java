26f004504014392cf05a8aa2f71d9df7553dc7ec
usuario <usuario@pc21-100>
2023-10-03 17:33:11 +0200

    Commit Faltan los Test

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -1,5 +1,41 @@
 package pkg;
 
-public class Empleado {
-
+public class Empleado {	
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	
+	public float calculoNominaBruta(TipoEmpleado tipoEmpleado, float ventasMes, float horasExtra) {
+		float salarioBase = 0, primas = 0, extras = 0;
+		
+		//Compruebo el Tipo de Empleado
+		if(tipoEmpleado == TipoEmpleado.Vendedor) {
+			salarioBase = 2000;
+		}else if(tipoEmpleado == TipoEmpleado.Encargado) {
+			salarioBase = 2500;
+		}
+		
+		//Compruebo y devuelvo el Salario de Nomina Bruta
+		if(ventasMes>=1500) {
+				primas = 200;
+		}else if(ventasMes>=1000){
+				primas = 100;
+		}
+		
+		//Asigno las horas extras
+		extras = 30*horasExtra;
+		
+		return salarioBase + primas + extras;
+		
+	}
+	
+	public float calculoNominaNeta(float nominaBruta) {
+		float retencion = 0;
+		
+		if(nominaBruta>2500) {
+			retencion=0.18f;
+		}else if(nominaBruta>2100) {
+			retencion = 0.15f;
+		}
+		return nominaBruta*(1-retencion);
+	}
 }
---
+++ src/test/EmpleadoTest.java
@@ -0,0 +1,112 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpleadoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCalculoNominaBrutaEncargado() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVendedor() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaOtro() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesNegativa() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesMenor100() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesEntre1000Y1500() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesMayor1500() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaHorasExtrasPositivas() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+	
+	@Test
+	void testCalculoNominaBrutaHorasExtrasNegativas() {
+		fail("Not yet implemented");
+		
+		//4 pruebas hay que hacer
+	}
+
+	@Test
+	void testCalculoNominaNetaNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMenor2100() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaEntre2100y2500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMayor2500() {
+		fail("Not yet implemented");
+	}
+
+}
